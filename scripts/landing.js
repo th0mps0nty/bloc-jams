ba245447cafbacb1db5ccd11d82dbc302b90b1aa
Tyler Thompson <th0mps0n.ty@protonmail.com>
2017-04-18 20:20:24 -0400

    Checkpoint-16 Complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,24 @@
-var pointsArray = document.getElementsByClassName('point');
- 
-var revealPoint = function(point) {
-        point.style.opacity = 1;
-        point.style.transform = "scaleX(1) translateY(0)";
-        point.style.msTransform = "scaleX(1) translateY(0)";
-        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
     };
+    $.each($('.point'), revealPoint);
+};
 
-var animatePoints = function (points) {
-        forEach(points, revealPoint);
-    };
+$(window).load(function() {
 
-window.onload = function() {
-         // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-          if (window.innerHeight > 950) {
-              animatePoints(pointsArray);
-          }
-     
-          window.addEventListener('scroll', function(event) {
-            if (pointsArray[0].getBoundingClientRect().top <= 500) {
-                animatePoints(pointsArray);   
-         }
+    if ($(window).height() > 950) {
+        animatePoints();
+    }
+
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
+        }
     });
-}
+});
